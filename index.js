a57a0136e0e853bade932371b1c40468e551d44b
abhishek kaushal <abhikaushalak17@gmail.com>
2024-03-01 17:24:12 +0530

    564738

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = 28031;
 
 const connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
