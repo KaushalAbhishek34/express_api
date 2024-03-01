554eaa5fe045c1728293194d7eba444eafdbfbd4
abhishek kaushal <abhikaushalak17@gmail.com>
2024-03-02 01:35:18 +0530

    rfgh

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const connection = mysql.createConnection({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE
+    database: process.env.MYSQLDATABASE,
+    connectTimeout: 60000 
 });
 
 connection.connect((err) => {
