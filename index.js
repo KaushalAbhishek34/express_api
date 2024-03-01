314a37de631df0d6a04b585bb8dfb3c804b86998
abhishek kaushal <abhikaushalak17@gmail.com>
2024-03-02 00:32:37 +0530

    rtghuytgfd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 console.log("Connecting to MySQL database...");
 const connection = mysql.createConnection({
