05aa18a00c98d15b0d67510fb4c2b9eb1cd4fec4
abhishek kaushal <abhikaushalak17@gmail.com>
2024-03-05 13:16:19 +0530

    hncn

--- index.js
+++ index.js
@@ -63,11 +63,7 @@ app.get('/products/:id', (req, res) => {
             console.error(`Error fetching product with ID ${productId}:`, error);
             res.status(500).json({ error: 'Internal server error' });
         } else {
-            if (results.length === 0) {
-                res.status(404).json({ error: 'Product not found' });
-            } else {
                 res.json(results[0]);
-            }
         }
     });
 });
